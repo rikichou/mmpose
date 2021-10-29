3dad8310420e839da70e9fbab75cdb137d21ed76
ruiming <zhourui@streamax.com>
2021-10-29 12:23:03 +0800

    dataset info

--- configs/smoke_keypoint/litehrnet_30_smoke_keypoint_256x256.py
+++ configs/smoke_keypoint/litehrnet_30_smoke_keypoint_256x256.py
@@ -124,25 +124,73 @@ val_pipeline = [
         meta_keys=['image_file', 'center', 'scale', 'rotation', 'flip_pairs']),
 ]
 
+dataset_info = dict(
+    dataset_name='smoke_keypoint',
+    paper_info=dict(
+        author='Streamax',
+        title='2D smoke keypoints',
+        container='W',
+        year='2021',
+        homepage='www.github.com',
+    ),
+    keypoint_info={
+        0:
+        dict(
+            name='smoke_mouth',
+            id=0,
+            color=[255, 0, 0],
+            type='lower',
+            swap=''),
+        1:
+        dict(
+            name='smoke_middle',
+            id=1,
+            color=[0, 255, 0],
+            type='lower',
+            swap=''),
+        2:
+        dict(
+            name='smoke_end',
+            id=2,
+            color=[0, 0, 255],
+            type='lower',
+            swap=''),
+    },
+    skeleton_info={
+        0:
+        dict(link=('smoke_mouth', 'smoke_middle'), id=0, color=[255, 128, 0]),
+        1:
+        dict(link=('smoke_middle', 'smoke_end'), id=1, color=[255, 128, 0]),
+    },
+    joint_weights=[
+        1,1,1
+    ],
+    # Adapted from COCO dataset.
+    sigmas=[
+        0.089, 0.083, 0.083
+    ])
+
 data_root = 'data/smoke_keypoint_mpii'
 data = dict(
     samples_per_gpu=32,
     workers_per_gpu=2,
     train=dict(
         type='TopDownMpiiDataset',
-        dataset_info='configs/top_down/lite_hrnet/smoke_keypoint/smoke_keypoint_datainfo.py',
+        dataset_info=dataset_info,
         ann_file=f'{data_root}/annotations/train.json',
         img_prefix=f'{data_root}/images/',
         data_cfg=data_cfg,
         pipeline=train_pipeline),
     val=dict(
         type='TopDownMpiiDataset',
+        dataset_info=dataset_info,
         ann_file=f'{data_root}/annotations/val.json',
         img_prefix=f'{data_root}/images/',
         data_cfg=data_cfg,
         pipeline=val_pipeline),
     test=dict(
         type='TopDownMpiiDataset',
+        dataset_info=dataset_info,
         ann_file=f'{data_root}/annotations/val.json',
         img_prefix=f'{data_root}/images/',
         data_cfg=data_cfg,
